c305d5dc4a102ab7ab786b9403c89a696ec33ce6
zairl23 <756127792@qq.com>
2016-03-02 16:26:44 +0800

    add

---
+++ README.md
@@ -0,0 +1,12 @@
+Hapi-bone
+===========
+  Build micro-service base on hapi.
+
+## Follow
+  http://hapijs.com/tutorials
+
+## Source
+  https://github.com/hapijs/hapi
+
+## Plugins
+  http://hapijs.com/tutorials/plugins
---
+++ app.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+const server = new Hapi.Server();
+server.connection({ port: 3000 });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, world!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: function (request, reply) {
+            reply.file('./public/hello.html');
+        }
+    });
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: [{
+            reporter: require('good-console'),
+            events: {
+                response: '*',
+                log: '*'
+            }
+        }]
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+      if (err) {
+        throw err;
+      }
+      console.log('Server running at:', server.info.uri);
+    });
+  });
